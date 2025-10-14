bea77a9e03f92ab1387e6727b3975d62c3e38718
shahalafathima213-alt <shahalafathima213@gmail.com>
2025-10-14 19:58:51 +0530

    toLowerCase completed

---
+++ toLowerCase.js
@@ -0,0 +1,20 @@
+// 709. To Lower Case
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+console.log(toLowerCase("HELLO"));
