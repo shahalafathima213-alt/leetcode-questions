f5d14deb4a5dcd15e52369379643b1f68ae050c5
shahalafathima213-alt <shahalafathima213@gmail.com>
2025-11-11 09:24:01 +0530

    palindromeLength

---
+++ PalindromeLength.js
@@ -0,0 +1,34 @@
+var longestPalindrome = function(s) {
+    
+  if (s.length <= 1) return s;
+
+  let start = 0, maxLength = 1;
+
+  
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+   
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+   
+    let len1 = expandAroundCenter(i, i);
+   
+    let len2 = expandAroundCenter(i, i + 1);
+
+    let len = Math.max(len1, len2);
+
+    if (len > maxLength) {
+      maxLength = len;
+      start = i - Math.floor((len - 1) / 2);
+    }
+  }
+
+  return s.substring(start, start + maxLength);
+}
+
+console.log(longestPalindrome('babad'))
